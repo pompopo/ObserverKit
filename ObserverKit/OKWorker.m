65a0a09efe5784f2895d60679e354b2ba28ff3bd
pompopo <pompopo@gmail.com>
2014-11-17 22:10:02 +0900

    Unpack NSValue or NSNumber object(KVO)

--- ObserverKit/OKWorker.m
+++ ObserverKit/OKWorker.m
@@ -3,7 +3,10 @@
 //
 
 #import "OKWorker.h"
-
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "NSMethodSignatureForBlock.m"
+typedef void(^OKOneArgumentBlock)(id);
 typedef void(^OKTwoArgumentsBlock)(id, id);
 typedef void(^OKThreeArgumentsBlock)(id, id, id);
 typedef void(^OKFourArgumentsBlock)(id, id, id, id);
@@ -24,11 +27,71 @@ typedef void(^OKFourArgumentsBlock)(id, id, id, id);
                       ofObject:(id)object
                         change:(NSDictionary *)change
                        context:(void *)context {
-    OKFourArgumentsBlock block = self.block;
-    block(self.owner, change[NSKeyValueChangeNewKey], change[NSKeyValueChangeOldKey], keyPath);
+
+    NSMethodSignature *signature = NSMethodSignatureForBlock(self.block);
+    id newValue = change[NSKeyValueChangeNewKey];
+    id oldValue = change[NSKeyValueChangeOldKey];
+    #define call_block(type, arg1, arg2) (^(){ \
+                void(^block)(id, type, type, id); \
+                block = self.block; \
+                block(self.owner, arg1, arg2, keyPath); \
+            })()
+
+
+    if (signature.numberOfArguments > 2) {
+        const char *type = [signature getArgumentTypeAtIndex:2];
+        if (strcmp(type, @encode(id)) == 0) {
+            call_block(id, newValue, oldValue);
+        } else if (strcmp(type, @encode(BOOL)) == 0) {
+            call_block(BOOL, [newValue boolValue], [oldValue boolValue]);
+        } else if (strcmp(type, @encode(char)) == 0) {
+            call_block(char, [newValue charValue], [oldValue charValue]);
+        } else if (strcmp(type, @encode(double)) == 0) {
+            call_block(double, [newValue doubleValue], [oldValue doubleValue]);
+        } else if (strcmp(type, @encode(float)) == 0) {
+            call_block(float, [newValue floatValue], [oldValue floatValue]);
+        } else if (strcmp(type, @encode(int)) == 0) {
+            call_block(int, [newValue intValue], [oldValue intValue]);
+        } else if (strcmp(type, @encode(NSInteger)) == 0) {
+            call_block(NSInteger, [newValue integerValue], [oldValue integerValue]);
+        } else if (strcmp(type, @encode(long long)) == 0) {
+            call_block(long long, [newValue longLongValue], [oldValue longLongValue]);
+        } else if (strcmp(type, @encode(long)) == 0) {
+            call_block(long, [newValue longValue], [oldValue longValue]);
+        } else if (strcmp(type, @encode(short)) == 0) {
+            call_block(short, [newValue shortValue], [oldValue shortValue]);
+        } else if (strcmp(type, @encode(unsigned char)) == 0) {
+            call_block(unsigned char, [newValue unsignedCharValue], [oldValue unsignedCharValue]);
+        } else if (strcmp(type, @encode(NSUInteger)) == 0) {
+            call_block(NSUInteger, [newValue unsignedIntegerValue], [oldValue unsignedIntegerValue]);
+        } else if (strcmp(type, @encode(unsigned int)) == 0) {
+            call_block(unsigned int, [newValue unsignedIntValue], [oldValue unsignedIntValue]);
+        } else if (strcmp(type, @encode(unsigned long long)) == 0) {
+            call_block(unsigned long long, [newValue unsignedLongLongValue], [oldValue unsignedLongLongValue]);
+        } else if (strcmp(type, @encode(unsigned long)) == 0) {
+            call_block(unsigned long, [newValue unsignedLongValue], [oldValue unsignedLongValue]);
+        } else if (strcmp(type, @encode(unsigned short)) == 0) {
+            call_block(unsigned short, [newValue unsignedShortValue], [oldValue unsignedShortValue]);
+        } else if (strcmp(type, @encode(CGPoint)) == 0) {
+            call_block(CGPoint, [newValue CGPointValue], [oldValue CGPointValue]);
+        } else if (strcmp(type, @encode(CGSize)) == 0) {
+            call_block(CGSize, [newValue CGSizeValue], [oldValue CGSizeValue]);
+        } else if (strcmp(type, @encode(CGRect)) == 0) {
+            call_block(CGRect, [newValue CGRectValue], [oldValue CGRectValue]);
+        } else {
+            // Unsupported type
+            OKFourArgumentsBlock block = self.block;
+            block(self.owner, newValue, oldValue, keyPath);
+        }
+    } else {
+        OKOneArgumentBlock block = self.block;
+        block(self.owner);
+    }
 }
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
+
+
 @end
--- ObserverKitDemo/ObserverKitDemoTests/ObserverKitDemoTests.m
+++ ObserverKitDemo/ObserverKitDemoTests/ObserverKitDemoTests.m
@@ -14,6 +14,25 @@
 @property (nonatomic) NSInteger i;
 @property (nonatomic) NSInteger j;
 @property (nonatomic) BOOL done;
+
+@property (nonatomic) id idVar;
+@property (nonatomic) BOOL boolVar;
+@property (nonatomic) char charVar;
+@property (nonatomic) double doubleVar;
+@property (nonatomic) float floatVar;
+@property (nonatomic) int intVar;
+@property (nonatomic) NSInteger integerVar;
+@property (nonatomic) long long longLongVar;
+@property (nonatomic) long longVar;
+@property (nonatomic) short shortVar;
+@property (nonatomic) unsigned char unsignedCharVar;
+@property (nonatomic) NSUInteger unsignedIntegerVar;
+@property (nonatomic) unsigned long long unsignedLongLongVar;
+@property (nonatomic) unsigned long unsignedLongVar;
+@property (nonatomic) unsigned short unsignedShortVar;
+@property (nonatomic) CGPoint CGPointVar;
+@property (nonatomic) CGSize CGSizeVar;
+@property (nonatomic) CGRect CGRectVar;
 @end
 
 @implementation ObserverKitDemoTests
@@ -30,11 +49,14 @@
 - (void)testKeyPath {
     self.i = 100;
     self.j = 200;
+    __block int numberOfTests = 0;
+
     self.ok_observer.keyPath(@"i", ^(typeof(self)me, id newVal, id oldVal, NSString *path) {
         XCTAssert(self == me);
         XCTAssert([newVal integerValue] == 101);
         XCTAssert([oldVal integerValue] == 100);
         XCTAssert([path isEqualToString:@"i"]);
+        numberOfTests++;
     }).keyPath(@[@"i", @"j"], ^(id me, id newVal, id oldVal, NSString *path) {
         XCTAssert(self == me);
         if ([path isEqualToString:@"j"]) {
@@ -46,11 +68,102 @@
         } else {
             XCTFail();
         }
+        numberOfTests++;
+
     });
     self.i = 101;
     self.j = 201;
+    while (numberOfTests != 3) {
+        sleep(1);
+    }
 }
 
+- (void)testCasts {
+    __block int numberOfTests = 0;
+    self.ok_observer.keyPath(@"idVar", ^(id me, id val) {
+        XCTAssert(val == self.idVar);
+        numberOfTests++;
+    }).keyPath(@"boolVar", ^(id me, BOOL val) {
+        XCTAssert(val == YES);
+        numberOfTests++;
+    }).keyPath(@"charVar", ^(id me, char val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"doubleVar", ^(id me, double val) {
+        XCTAssert(val == 1.0);
+        numberOfTests++;
+    }).keyPath(@"floatVar", ^(id me, float val) {
+        XCTAssert(val == 1.0);
+        numberOfTests++;
+    }).keyPath(@"intVar", ^(id me, int val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"integerVar", ^(id me, NSInteger val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"longLongVar", ^(id me, long long val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"longVar", ^(id me, long val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"shortVar", ^(id me, short val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"unsignedCharVar", ^(id me, unsigned char val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"unsignedIntegerVar", ^(id me, NSUInteger val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"unsignedLongLongVar", ^(id me, unsigned long long val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"unsignedLongVar", ^(id me, unsigned long val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"unsignedShortVar", ^(id me, unsigned short val) {
+        XCTAssert(val == 1);
+        numberOfTests++;
+    }).keyPath(@"CGPointVar", ^(id me, CGPoint val) {
+        XCTAssert(val.x == 1);
+        XCTAssert(val.y == 1);
+        numberOfTests++;
+    }).keyPath(@"CGSizeVar", ^(id me, CGSize val) {
+        XCTAssert(val.height == 1);
+        XCTAssert(val.width == 1);
+        numberOfTests++;
+    }).keyPath(@"CGRectVar", ^(id me, CGRect val) {
+        XCTAssert(val.origin.x == 1);
+        XCTAssert(val.origin.y == 1);
+        XCTAssert(val.size.height == 1);
+        XCTAssert(val.size.width == 1);
+        numberOfTests++;
+    });
+
+    self.idVar = @1;
+    self.boolVar = YES;
+    self.charVar = 1;
+    self.doubleVar = 1.0;
+    self.floatVar = 1.0;
+    self.intVar = 1;
+    self.integerVar = 1;
+    self.longLongVar = 1;
+    self.longVar = 1;
+    self.shortVar = 1;
+    self.unsignedCharVar = 1;
+    self.unsignedIntegerVar = 1;
+    self.unsignedLongLongVar = 1;
+    self.unsignedLongVar = 1;
+    self.unsignedShortVar = 1;
+    self.CGPointVar = CGPointMake(1, 1);
+    self.CGSizeVar = CGSizeMake(1, 1);
+    self.CGRectVar = CGRectMake(1, 1, 1, 1);
+    while (numberOfTests != 18) {
+        sleep(1);
+    }
+    
+}
 - (void)testControl {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     self.ok_observer.control(button, UIControlEventTouchUpInside, ^(typeof(self) me, UIControl *control) {
